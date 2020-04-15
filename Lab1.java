34b98b365e44b6653a8b41d0e9ed1543a76ff0e0
miriyandasenov <63752573+miriyandasenov@users.noreply.github.com>
2020-04-16 01:13:29 +0300

    Add file Lab1.java

---
+++ Lab1.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Lab1
+{
+
+	public static void main(String[] args)
+	{
+		
+			Scanner sc = new Scanner(System.in);
+			
+			String name = sc.nextLine();
+			int age = Integer.parseInt( sc.nextLine() );
+			double grade = Double.parseDouble( sc.nextLine() );
+			
+			System.out.printf("Name: %s, Age: %d, Grade: %.2f", name, age, grade);
+	}
+
+}
