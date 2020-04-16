4e82ecc90668be67c6ee48711247014f7260d29e
miriyandasenov <63752573+miriyandasenov@users.noreply.github.com>
2020-04-17 01:41:22 +0300

    Passed

---
+++ Lab2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Lab2
+{
+
+	public static void main(String[] args)
+	{
+		
+			Scanner sc = new Scanner(System.in);
+			
+			double grade = Double.parseDouble( sc.nextLine() );
+			
+			if(grade >= 3.0)
+			
+			System.out.println("Passed!");
+	}
+
+}
